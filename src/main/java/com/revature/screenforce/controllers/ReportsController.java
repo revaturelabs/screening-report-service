378a86344662b60178c0aafec037009c7066f0bb
tkgordon <travis_gordon@att.net>
2019-04-18 20:55:30 -0400

    Rest dependencies and small changes

--- src/main/java/com/revature/screenforce/controllers/ReportsController.java
+++ src/main/java/com/revature/screenforce/controllers/ReportsController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 import com.revature.screenforce.beans.QuestionScore;
 import com.revature.screenforce.beans.Screening;
 import com.revature.screenforce.beans.SoftSkillViolation;

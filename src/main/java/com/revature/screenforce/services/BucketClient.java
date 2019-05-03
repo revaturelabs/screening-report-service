7994ec5e7ca787ee20bb9e11c3c568f527b4e105
tkgordon <travis_gordon@att.net>
2019-05-03 08:45:46 -0400

    Final changes for the presentation, including one hot fix for the feign client bucket

--- src/main/java/com/revature/screenforce/services/BucketClient.java
+++ src/main/java/com/revature/screenforce/services/BucketClient.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.revature.screenforce.beans.Bucket;
 import com.revature.screenforce.beans.Question;
 
+import feign.RequestLine;
+
 public interface BucketClient {
+	@RequestLine("GET /{bucketId}")
 	public Bucket getBucketById(@PathVariable Integer bucketId);
 }

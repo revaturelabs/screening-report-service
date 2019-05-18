b4835b66a5fd50cf040c090fed3c54ba26156109
Emily Higgins <emilyrchiggins@gmail.com>
2019-05-18 18:00:09 -0400

    refactor config files, add @EnableDiscoveryClient

--- src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
+++ src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
@@ -2,9 +2,11 @@ package com.revature.screenforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import feign.FeignClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 //@EnableFeignClients
 public class ScreeningReportServiceApplication {
 

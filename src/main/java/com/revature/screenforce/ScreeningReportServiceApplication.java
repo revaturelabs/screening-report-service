f03469e3e9a0c33dc94bb963994aab8d10a3608f
william oleson <wnoleson@gmail.com>
2019-05-23 16:15:52 -0400

    FeignClient setup

--- src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
+++ src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import feign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableFeignClients
+@EnableFeignClients
 public class ScreeningReportServiceApplication {
 
 	public static void main(String[] args) {

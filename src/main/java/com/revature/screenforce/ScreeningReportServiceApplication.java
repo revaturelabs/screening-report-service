56dbc6859a74c39a5d5f308adfca90e2c2e810d3
Em <ebaillie27@gmail.com>
2019-05-31 08:21:46 -0400

    @enableswagger missing above class

--- src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
+++ src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
@@ -1,20 +1,23 @@
 package com.revature.screenforce;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-import static springfox.documentation.builders.PathSelectors.regex;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 //import feign.FeignClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableSwagger2
 public class ScreeningReportServiceApplication {
 
 	public static void main(String[] args) {

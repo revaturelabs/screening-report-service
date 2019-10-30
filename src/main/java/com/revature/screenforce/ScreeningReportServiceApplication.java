42f24b8de226aa17821914d118d9059d04a7f0ae
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-30 16:40:51 -0400

    made some changes to the app class

--- src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
+++ src/main/java/com/revature/screenforce/ScreeningReportServiceApplication.java
@@ -31,7 +31,10 @@ public class ScreeningReportServiceApplication {
 	 */
 	@Bean
 	public Docket swag() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 }
